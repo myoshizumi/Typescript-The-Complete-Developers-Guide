19f3559afe90f9123ce656c3c193dc388268645f
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-03-09 13:25:39 +0900

    Separating Swapping and Comparison

--- build/index.js
+++ build/index.js
@@ -1,4 +1,8 @@
 "use strict";
-const sorter = new Sorter([10, 3, -5, 0]);
+Object.defineProperty(exports, "__esModule", { value: true });
+const Sorter_1 = require("./Sorter");
+const NumbersCollection_1 = require("./NumbersCollection");
+const numbersCollection = new NumbersCollection_1.NumbersCollection([10, 30, -5, 0]);
+const sorter = new Sorter_1.Sorter(numbersCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log(numbersCollection);
