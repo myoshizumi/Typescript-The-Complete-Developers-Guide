41c1f31d685d549ea962b2b2df335fe5f89ba665
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-03-09 13:01:50 +0900

    Extracting Key Logic

--- build/index.js
+++ build/index.js
@@ -1,29 +1,4 @@
 "use strict";
-class Sorter {
-    constructor(collection) {
-        this.collection = collection;
-    }
-    sort() {
-        const { length } = this.collection;
-        for (let i = 0; i < length; i++) {
-            for (let j = 0; j < length - i - 1; j++) {
-                // All of this only works if collection is number[]
-                // if collection is an arrayy of numbers
-                if (this.collection instanceof Array) {
-                    if (this.collection[j] > this.collection[j + 1]) {
-                        const leftHand = this.collection[j];
-                        this.collection[j] = this.collection[j + 1];
-                        this.collection[j + 1] = leftHand;
-                    }
-                }
-                // Only going to work if collection is a string
-                // if collection is a string, do this logic instead
-                if (typeof this.collection === "string") {
-                }
-            }
-        }
-    }
-}
 const sorter = new Sorter([10, 3, -5, 0]);
 sorter.sort();
 console.log(sorter.collection);
