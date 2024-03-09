48e2c816801818c7a534ef5c0bf1a641b87ff333
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-03-09 18:43:46 +0900

    Issues with Inheritance

--- src/Sorter.ts
+++ src/Sorter.ts
@@ -4,15 +4,13 @@ interface Sortable {
 	swap(leftHandle: number, rightHandle: number): void;
 }
 export class Sorter {
-	constructor(public collection: Sortable) {}
-
 	sort(): void {
-		const { length } = this.collection;
+		const { length } = this;
 
 		for (let i = 0; i < length; i++) {
 			for (let j = 0; j < length - i - 1; j++) {
-				if (this.collection.compare(j, j + 1)) {
-					this.collection.swap(j, j + 1);
+				if (this.compare(j, j + 1)) {
+					this.swap(j, j + 1);
 				}
 			}
 		}
