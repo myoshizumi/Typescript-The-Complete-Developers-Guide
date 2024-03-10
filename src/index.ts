bde5e02a3c68afc088e1c11763dafc7854781425
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-03-10 16:30:45 +0900

    Type Assertions

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,11 @@
 import { CsvFileReader } from "./CsvFileReader";
+import { MatchResult } from "./MatchResult";
 
 const reader = new CsvFileReader("football.csv");
 reader.read();
 
 console.log(reader.read());
 
-// enum - enumeration
-enum MatchResult {
-	HomeWin = "H",
-	AwayWin = "A",
-	Draw = "D",
-}
 // const printMachResult = (): MatchResult => {
 //     if (match[5] === 'H') {
 //         return MatchResult.HomeWin;
