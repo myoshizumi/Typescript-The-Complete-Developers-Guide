27e46835546b4767922d269f0853f1a1c5890992
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-03-10 11:37:53 +0900

    Solving All Issues with Abstract Classes

--- src/index.ts
+++ src/index.ts
@@ -3,24 +3,22 @@ import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
 import { LinkedList } from "./LinkedList";
 
-const numbersCollection = new NumbersCollection([10, 30, -5, 110]);
-numbersCollection.sort();
+// const numbersCollection = new NumbersCollection([10, 30, -5, 110]);
+// numbersCollection.sort();
 
 // const sorter = new Sorter(numbersCollection);
 // sorter.sort();
-// console.log(numbersCollection);
+// console.log(numbersCollection.data);
 
 // const charactersCollection = new CharactersCollection("iHPkdf");
-// const sorter = new Sorter(charactersCollection);
-// sorter.sort();
+// charactersCollection.sort();
 // console.log(charactersCollection);
 
 const linkedList = new LinkedList();
-linkedList.add(-500);
+linkedList.add(500);
 linkedList.add(-10);
 linkedList.add(-3);
 linkedList.add(4);
 
-// const sorter = new Sorter(linkedList);
-// sorter.sort();
-// linkedList.print();
+linkedList.sort();
+linkedList.print();
