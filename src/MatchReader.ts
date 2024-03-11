0ef9e5ee7a9ed80681d33676601c02b3ac12da5c
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-03-11 22:20:07 +0900

    Applying a Type to a Generic Class

--- src/MatchReader.ts
+++ src/MatchReader.ts
@@ -2,8 +2,10 @@ import { CsvFileReader } from "./CsvFileReader";
 import { dateStringToDate } from "./utils";
 import { MatchResult } from "./MatchResult";
 
-export class MatchReader extends CsvFileReader {
-	mapRow(row: string): MatchData {
+type MatchData = [Date, string, string, number, number, MatchResult, string];
+
+export class MatchReader extends CsvFileReader<MatchData> {
+	mapRow(row: string[]): MatchData {
 		return [
 			dateStringToDate(row[0]),
 			row[1],
