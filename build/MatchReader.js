b2a6259e2aea92b16767e49e2b8ad37de2a70618
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-03-12 23:50:38 +0900

    Transforming Data

--- build/MatchReader.js
+++ build/MatchReader.js
@@ -1,7 +1,23 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const utils_1 = require("./utils");
 class MatchReader {
     constructor(reader) {
         this.reader = reader;
+        this.matches = [];
+    }
+    load() {
+        this.reader.read();
+        this.matches = this.reader.data.map((row) => {
+            return [
+                (0, utils_1.dateStringToDate)(row[0]),
+                row[1],
+                row[2],
+                parseInt(row[3]),
+                parseInt(row[4]),
+                row[5],
+                row[6],
+            ];
+        });
     }
 }
