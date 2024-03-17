9759f104a9baaaf679495bcf1ebc2dead76a70ca
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-03-17 12:28:09 +0900

    Refactoring with Composition

--- src/models/User.ts
+++ src/models/User.ts
@@ -5,11 +5,7 @@ interface UserProps {
 	age?: number;
 }
 
-type Callback = () => void;
-
 export class User {
-	events: { [key: string]: Callback[] } = {};
-
 	constructor(private data: UserProps) {}
 
 	get(propName: string): number | string {
@@ -20,37 +16,20 @@ export class User {
 		Object.assign(this.data, update);
 	}
 
-	on(eventName: string, callback: Callback): void {
-		const handlers = this.events[eventName] || [];
-		handlers.push(callback);
-		this.events[eventName] = handlers;
-	}
-
-	trigger(eventName: string): void {
-		const handlers = this.events[eventName];
-
-		if (!handlers || handlers.length === 0) {
-			return;
-		}
-
-		handlers.forEach((callback) => {
-			callback();
-		});
-	}
 	fetch(): void {
 		axios
 			.get(`http://localhost:3000/users/${this.get("id")}`)
 			.then((response: AxiosResponse): void => {
 				this.set(response.data);
 			});
-  }
-  
-  save(): void  {
-    const id = this.get("id");
-    if (id) {
-      axios.put(`http://localhost:3000/users/${id}`, this.data)
-    } else {
-      axios.post(`http://localhost:3000/users`, this.data)
-    }
-  }
+	}
+
+	save(): void {
+		const id = this.get("id");
+		if (id) {
+			axios.put(`http://localhost:3000/users/${id}`, this.data);
+		} else {
+			axios.post(`http://localhost:3000/users`, this.data);
+		}
+	}
 }
