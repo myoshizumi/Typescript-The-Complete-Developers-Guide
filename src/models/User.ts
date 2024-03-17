bbe8f12f444d5a38b3dbcf4b485b5ee822a48950
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-03-17 12:04:23 +0900

    Adding Fetch Functionality

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,38 +1,47 @@
+import axios, { AxiosResponse } from "axios";
 interface UserProps {
-  name?: string;
-  age?: number;
+	id?: number;
+	name?: string;
+	age?: number;
 }
 
 type Callback = () => void;
 
 export class User {
-  events: { [key: string]: Callback[] } = {};
+	events: { [key: string]: Callback[] } = {};
 
-  constructor(private data: UserProps) {}
+	constructor(private data: UserProps) {}
 
-  get(propName: string): number | string {
-    return this.data[propName];
-  }
+	get(propName: string): number | string {
+		return this.data[propName];
+	}
 
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
-  }
+	set(update: UserProps): void {
+		Object.assign(this.data, update);
+	}
 
-  on(eventName: string, callback: Callback): void {
-    const handlers = this.events[eventName] || [];
-    handlers.push(callback);
-    this.events[eventName] = handlers;
-  }
+	on(eventName: string, callback: Callback): void {
+		const handlers = this.events[eventName] || [];
+		handlers.push(callback);
+		this.events[eventName] = handlers;
+	}
 
-  trigger(eventName: string): void {
-    const handlers = this.events[eventName];
+	trigger(eventName: string): void {
+		const handlers = this.events[eventName];
 
-    if (!handlers || handlers.length === 0) {
-      return;
-    }
+		if (!handlers || handlers.length === 0) {
+			return;
+		}
 
-    handlers.forEach(callback => {
-      callback();
-    });
-  }
+		handlers.forEach((callback) => {
+			callback();
+		});
+	}
+	fetch(): void {
+		axios
+			.get(`http://localhost:3000/users/${this.get("id")}`)
+			.then((response: AxiosResponse): void => {
+				this.set(response.data);
+			});
+	}
 }
