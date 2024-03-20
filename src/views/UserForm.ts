c0f2603930b27ffadf8c1c4755f9b4f34771cd77
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-03-20 16:01:37 +0900

    Building the UserForm

---
+++ src/views/UserForm.ts
@@ -0,0 +1,12 @@
+export class UserForm {
+	parent: Element;
+
+	template(): string {
+		return /*html*/ `
+            <div>
+                <h1>User Form</h1>
+                <input />
+            </div>
+        `;
+	}
+}
