3f60fbbf86fee43be64ffa98e9245d51d2da1808
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-03-23 17:07:01 +0900

    Using an Express Router

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,8 @@
 import express, { Request, Response } from "express";
-
+import { router } from "./routes/loginRoutes";
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
-	res.send(/*html*/ `
-        <div>
-            <h1>hi there!</h1>
-        </div>
-    `);
-});
+app.use(router);
 
 app.listen(3000, () => {
 	console.log("listening on port 3000");
