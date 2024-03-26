80212a9d45c3cc6d1baf422870e70fe41b44676f
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-03-26 23:27:22 +0900

    Simple Components

--- build/User.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var User = /** @class */ (function () {
-    function User() {
-    }
-    return User;
-}());
--- src/controllers/decorators/Methods.ts
+++
@@ -1,7 +0,0 @@
-export enum Methods {
-	get = "get",
-	put = "put",
-	post = "post",
-	patch = "patch",
-	del = "delete",
-}
